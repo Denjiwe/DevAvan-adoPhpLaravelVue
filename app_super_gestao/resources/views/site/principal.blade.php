b73b56f2c236a9a7ac4b4fda8ef10fa6034bf693
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2023-02-27 09:42:53 -0300

    aulas 27-30 módulo 5

---
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -0,0 +1,1 @@
+<h3>Sejá bem-vindo! (view)</h3>
--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -13,14 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* feito por callback
 Route::get('/', function () {
     return 'Olá, seja bem-vindo';
 });
+*/
 
-Route::get('/sobre-nos', function () {
-    return 'Sobre-nós';
-});
+// feito por controller
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
 
-Route::get('/contato', function () {
-    return 'Contato';
-});
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
