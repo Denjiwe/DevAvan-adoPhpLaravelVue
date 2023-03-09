786879387462a2a311b234f43a5a4a331224e428
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2023-03-09 15:57:40 -0300

    Aulas 89-100 do Módulo 8

--- app_super_gestao/app/Models/Fornecedor.php
+++ app_super_gestao/app/Models/Fornecedor.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    use HasFactory;
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
