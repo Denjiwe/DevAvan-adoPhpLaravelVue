7e59074025a8f7c31e06ce3c6e1626af1637f390
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2023-04-08 15:08:22 -0300

    Aulas 328-334 do Módulo 18

---
+++ app_locadora_carros/app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        $credenciais = $request->all(['email', 'password']);
+
+        // faz autenticação retornando o token de autorização
+        $token = auth('api')->attempt($credenciais);
+
+        if($token) { // autenticado com sucesso
+            return response()->json(['token' => $token]);
+        } else { // erro de senha ou email
+            return response()->json(['erro' => 'Usuário ou senha incorretos'], 403);
+
+            // 401 = Unauthorized -> não tem autorização do acesso
+            // 403 = Forbidden -> não tem autenticação
+        }
+    }
+
+    public function logout() {
+        return 'logout';
+    }
+
+    public function refresh() {
+        return 'refresh';
+    }
+
+    public function me() {
+        return 'me';
+    }
+}
--- app_locadora_carros/routes/api.php
+++ app_locadora_carros/routes/api.php
@@ -23,3 +23,8 @@ Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
 Route::apiResource('carro', 'App\Http\Controllers\CarroController');
 Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
 Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('me', 'App\Http\Controllers\AuthController@me');
