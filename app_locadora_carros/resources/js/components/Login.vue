8f827f396d4ba5902a58256ab0be56ff07841c5e
Santhiago Monteiro <santhiago.monteiro@hotmail.com>
2023-04-11 18:18:38 -0300

    Aulas 347-352 do Módulo 19

--- app_locadora_carros/resources/js/components/Login.vue
+++ app_locadora_carros/resources/js/components/Login.vue
@@ -6,13 +6,13 @@
                 <div class="card-header">Login Vue</div>
 
                 <div class="card-body">
-                    <form method="POST" action="">
-
+                    <form method="POST" action="" @submit.prevent="login($event)">
+                        <input type="hidden" name="_token" :value="csrf_token">
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">Enail</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email" autofocus>
+                                <input id="email" type="email" class="form-control" name="email" required autocomplete="email" autofocus v-model="email">
                             </div>
                         </div>
 
@@ -20,7 +20,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-end">Senha</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password" v-model="password">
                             </div>
                         </div>
 
@@ -55,3 +55,37 @@
     </div>
 </div>
 </template>
+
+<script>
+    export default {
+        props: ['csrf_token'],
+        data() {
+            return {
+                email: '',
+                password: '' 
+            }
+        },
+        methods: {
+            login(e) {
+                let url = 'http://localhost:8000/api/login'
+                let configuracao = {
+                    method: 'post',
+                    body: new URLSearchParams({
+                        'email' : this.email,
+                        'password' : this.password
+                    })
+                }
+
+                fetch(url, configuracao)
+                    .then(Response => Response.json())
+                    .then(data => {
+                        if(data.token) {
+                            document.cookie = 'token='+data.token+';SameSite=Lax'
+                        }
+                        //dar sequência no envio do form de autenticação por sessão
+                        e.target.submit()
+                    })
+            }
+        }
+    }
+</script>
